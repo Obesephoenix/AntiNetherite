487d7164e9fdd125be64adb841d5264acc9cc8e3
Obesephoenix <john830beck@gmail.com>
2021-09-13 18:11:00 +0100

    Setup project using create-spigot-plugin

---
+++ src/main/java/net/obesephoenix/antinetherite/AntiNetherite.java
@@ -0,0 +1,17 @@
+package net.obesephoenix.antinetherite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiNetherite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
